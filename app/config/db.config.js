f9ab0ab18c4672358e7c3fc9173490d8ee5be794
soravitpuengsema <s6301012630184@email.kmutnb.ac.th>
2022-03-14 13:25:16 +0700

    heroku db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "xogame",
+    HOST: "us-cdbr-east-05.cleardb.net",
+    USER: "b20a05d04457ee",
+    PASSWORD: "9e732fa5",
+    DB: "heroku_5bb93df9e4fe82b",
     dialect: "mysql",
     pool: {
         max: 5,
